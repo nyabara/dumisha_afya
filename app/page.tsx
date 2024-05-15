4d25e1c3db8e564e1f5d852705ceb8240aaa5992
nyabara <nyabarayoma1@gmail.com>
2024-05-15 17:26:12 +0300

    trouble

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape} />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Dumisha Afya.</strong> This is the recruitment app for{' '}
+            <a href="" className="text-blue-500">
+              Dumisha Afya
             </a>
-            , brought to you by Vercel.
+            , brought to you by USAID.
           </p>
           <Link
             href="/login"
